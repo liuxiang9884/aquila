609ed8355b3727cf6bda2033e1865f990ccbeb3b
liuxiang <liuxiang9884@gmail.com>
2026-04-29 10:38:08 +0000

    core: tune book ticker layout

--- core/common/constants.h
+++ core/common/constants.h
@@ -1,7 +1,11 @@
 #ifndef AQUILA_CORE_COMMON_CONSTANTS_H_
 #define AQUILA_CORE_COMMON_CONSTANTS_H_
 
+#include <cstddef>
+
 namespace aquila {
+
+inline constexpr std::size_t kCacheLineBytes = 64;
 
 }  // namespace aquila
 
